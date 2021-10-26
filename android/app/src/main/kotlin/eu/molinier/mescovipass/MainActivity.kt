6d806345a82d2d268d9b50dc0968022cddd661f9
Lionel Molinier <lionel@molinier.eu>
2021-10-26 12:01:32 +0200

    first commit

---
+++ android/app/src/main/kotlin/eu/molinier/mescovipass/MainActivity.kt
@@ -0,0 +1,6 @@
+package eu.molinier.mescovipass
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/certificate.dart
@@ -0,0 +1,99 @@
+import 'package:eudcc/eudcc.dart';
+import 'package:expandable/expandable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class CertificateWidget extends StatefulWidget {
+  const CertificateWidget(this.cert, {Key? key}) : super(key: key);
+
+  final EUDigitalCovidCertificate cert;
+
+  @override
+  State<CertificateWidget> createState() => CertificateWidgetState();
+}
+
+class CertificateWidgetState extends State<CertificateWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final cert = widget.cert;
+    return Container(
+      //width: MediaQuery.of(context).size.width,
+      margin: const EdgeInsets.all(5),
+      decoration: BoxDecoration(color: Colors.white, boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.5),
+          spreadRadius: 5,
+          blurRadius: 7,
+          offset: const Offset(0, 2),
+        ),
+      ]),
+      child: Padding(
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          children: [
+            Center(
+              child: Column(children: [
+                const CircleAvatar(
+                  child: Image(image: AssetImage('assets/avatar_man.png')),
+                  backgroundColor: Colors.transparent,
+                  radius: 40,
+                ),
+                Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: RichText(
+                      text: TextSpan(
+                          text: (cert.certificates.single.holder.givenName ??
+                                  "") +
+                              " " +
+                              cert.certificates.single.holder.firstName,
+                          style: const TextStyle(fontSize: 22)),
+                    )),
+                RichText(
+                    text: TextSpan(
+                        text: cert.certificates.single.holder.firstNameICAO9303,
+                        style: const TextStyle(fontSize: 10))),
+              ]),
+            ),
+            QrImage(
+              data: cert.qr!,
+              version: QrVersions.auto,
+              //size: MediaQuery.of(context).size.width,
+            ),
+            ExpandablePanel(
+                theme: const ExpandableThemeData(
+                  headerAlignment: ExpandablePanelHeaderAlignment.center,
+                  tapHeaderToExpand: true,
+                ),
+                header: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(children: [
+                      RichText(
+                          text: TextSpan(
+                              text: "delivered: ",
+                              style: const TextStyle(
+                                  fontSize: 12, fontWeight: FontWeight.bold),
+                              children: [
+                            TextSpan(
+                              text: cert.issuedAt.toString(),
+                              style: const TextStyle(
+                                  fontSize: 12, fontWeight: FontWeight.normal),
+                            )
+                          ])),
+                      RichText(
+                          text: TextSpan(
+                              text: cert.issuedAt.toString(),
+                              style: const TextStyle(fontSize: 10))),
+                    ])),
+                collapsed: Row(),
+                expanded: Row(
+                  children: [
+                    const Text("plop"),
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
